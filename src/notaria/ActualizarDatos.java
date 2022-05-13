f6401b28c9577d49d67f8f91e1691077141e82f0
lukenavarro <44287208+lukenavarro@users.noreply.github.com>
2022-05-13 18:26:46 +0200

    Algo Fran

--- src/notaria/ActualizarDatos.java
+++ src/notaria/ActualizarDatos.java
@@ -6,15 +6,15 @@ import java.sql.Statement;
 
 public class ActualizarDatos { 
     /**
-     * Método que actualiza los datos de un cliente de la tabla clientes dado el nombre del cliente.
+     * MÃ©todo que actualiza los datos de un cliente de la tabla clientes dado el nombre del cliente.
      * 
-     * El método hace lo siguiente:
-     * 1º Consulta si el nombre del cliente existe en la base de datos
-     * 2º Muestra por pantalla los datos del cliente seleccionado
-     * 3º Si existe tal cliente, le pide al usuario que inserte un nuevo nombre y un nuevo teléfono
-     * 4º Realiza la Query para updatear ese registro con los nuevos datos
+     * El mÃ©todo hace lo siguiente:
+     * 1Âº Consulta si el nombre del cliente existe en la base de datos
+     * 2Âº Muestra por pantalla los datos del cliente seleccionado
+     * 3Âº Si existe tal cliente, le pide al usuario que inserte un nuevo nombre y un nuevo telÃ©fono
+     * 4Âº Realiza la Query para updatear ese registro con los nuevos datos
      * 
-     * @param nomCli Nombre del cliente que viene dado por parámetros
+     * @param nomCli Nombre del cliente que viene dado por parÃ¡metros
      * @author Fran
      */
 	public void actualizarClientes(String nomCli) {
@@ -22,15 +22,30 @@ public class ActualizarDatos {
 		String query;
 		
         ConexionMysql mysql = new ConexionMysql();
-        Connection conn;
+        Connection conn = mysql.Conectar();
         Statement s;
         ResultSet rs;
+        SeleccionaDatosDeTabla sc =new SeleccionaDatosDeTabla();
+        String nombre;
+        String telefono;
         
         try{
-            
+            s= conn.createStatement();
+            if(sc.consultarNomCliente(nomCli)){
+                sc.consultarNomCliente(nomCli);
+                nombre=UtilConsola.leerCadena("Introduzca el nombre nuevo del cliente",true);
+                telefono = UtilConsola.leerCadena("Introduzca el telefono nuevo del cliente",true);
+                query = "Update clientes Set telefono = "+telefono+" where nombre = "+nomCli;
+                s.execute(query);
+                query = "Update clientes Set nombre = "+nombre+" where nombre = "+nomCli;
+                s.execute(query);
+            }else{
+                System.out.println("No existe dicho cliente");
+            }
         } catch(Exception e) { 
             System.err.println ("Se han encontrado errores.- "+e.toString());     
         } finally {    
-            //TODO realiza la desconexión de la base de datos.
+            //TODO realiza la desconexiÃ³n de la base de datos.
+            mysql.desconectar(conn);
         }
     }}
