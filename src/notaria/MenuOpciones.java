1330b4900ecf72561a41e986dee3083d47b3c313
Conra-26 <seggarcia3@gmail.com>
2022-05-13 17:42:21 +0200

    Creacion del menu de cada aplicacion.

--- src/notaria/MenuOpciones.java
+++ src/notaria/MenuOpciones.java
@@ -1,5 +1,7 @@
 package notaria;
 
+import java.util.Scanner;
+
 public class MenuOpciones { // clase que contien el menu de opciones
 
 	String msg = "****************Ingrese la Opcion****************";
@@ -27,15 +29,55 @@ public class MenuOpciones { // clase que contien el menu de opciones
 	 */
 	public int menuPrincipal() {
 		//Opcion que devuelve el método. Esta opción la actualiza el usuario cuando leemos lo que introduce por teclado.
+		Scanner scan = new Scanner(System.in);
 		int opcion = 0;
-		try {
+		boolean opcionCorrecta= false;
+		while (!opcionCorrecta) {
+			try {
 			//TODO sysos con el menu
-			
+			System.out.println("Introduzca el numero por teclado, que desee hacer. ");
 			//TODO leer por teclado lo que el user introduce
+			opcion=scan.nextInt();
 			
+			switch (opcion) {
+			case 1:
+				opcionCorrecta = true;
+				System.out.println("Crear Tablas");
+				break;
+			case 2:
+				opcionCorrecta = true;
+				System.out.println("Ingresar Datos");
+				break;
+			case 3:
+				opcionCorrecta = true;
+				System.out.println("Consulta general");
+				break;
+			case 4:
+				opcionCorrecta = true;
+				System.out.println("Consulta documento determinado");
+				break;
+			case 5:
+				opcionCorrecta = true;
+				System.out.println("Actualizacion cliente");
+				break;
+			case 6:
+				opcionCorrecta = true;
+				System.out.println("Eliminar tablas");
+				break;
+			case 7:
+				opcionCorrecta = true;
+				System.out.println("EXIT");
+				break;
+			default:
+				opcionCorrecta = false;
+				System.out.println("MAL");
+				break;
+			}
 		} catch (Exception e) {
 			//TODO si el usuario introduce algo que no es un número, se controla la excepción
+			System.out.println("FATAL");
 			
+		}
 		}
 		return opcion;
 	}
