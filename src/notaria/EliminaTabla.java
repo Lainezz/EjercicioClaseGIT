bb16b656b99d6779f1a69437188c9164091e5923
JosephhRa <joseph-rangel2001@hotmail.com>
2022-05-13 17:56:34 +0200

    Se ha añadido el metodo borrar

--- src/notaria/EliminaTabla.java
+++ src/notaria/EliminaTabla.java
@@ -1,44 +1,46 @@
 package notaria;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
 
-/**  
+/**
  * nombre Ejercicio 1 de tarea 11 
  * descripción: clase que permite eliminar las tablas con todos sus datos.
  * @author Angel León
  */
 
-public class EliminaTabla { 
-    
-	/**
-	 * M�todo que borra las tres tablas de la base de datos notarbd.
-	 * Las tablas a eliminar son escCli, clientes y escrituras
-	 * 
-	 * @author Joseph
-	 */
+public class EliminaTabla {
+
+    /**
+     * M�todo que borra las tres tablas de la base de datos notarbd.
+     * Las tablas a eliminar son escCli, clientes y escrituras
+     *
+     * @author Joseph
+     */
     public void borrar () {
         ConexionMysql mysql = new ConexionMysql();
-        Connection conn;
+        Connection conn = null;
         Statement s;
         try {
             //TODO establecer conexion a la base de datos y preparar las sentencias
-            conn = mysql.conectar();
+            conn = mysql.Conectar();
             s = conn.createStatement();
             //TODO sentencias para elminar las tablas de la base de datos
             String query = ("DROP TABLE escCli;" +
-                            "DROP TABLE clientes;" +
-                            "DROP TABLE escrituras;");
+                    "DROP TABLE clientes;" +
+                    "DROP TABLE escrituras;");
             s.executeUpdate(query);
 
-        	//Mensaje de confirmaci�n de que las tablas han sido eliminadas
-        	System.out.println ("\n *** Tablas eliminadas ***"); 
+            //Mensaje de confirmaci�n de que las tablas han sido eliminadas
+            System.out.println ("\n *** Tablas eliminadas ***");
         }catch (SQLException e) {
-            System.err.println ("Error al intentar borrar las tablas. "); 
+            System.err.println ("Error al intentar borrar las tablas. ");
+            
         } finally {
             //TODO realizar la desconexi�n de la base de datos
             mysql.desconectar(conn);
         }
-    } 
+    }
 } 
 
 
