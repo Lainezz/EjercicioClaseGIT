74c31c975b8f0b9aebf4f35d4de2e865d477ac95
Mind.sh4ke <albapargom@gmail.com>
2022-05-13 17:12:57 +0200

    Implementada la funcion al metodo ingresarDatos de la tabla InsertaDatosEnLaTabla

--- src/notaria/InsertaDatosEnLaTabla.java
+++ src/notaria/InsertaDatosEnLaTabla.java
@@ -1,11 +1,13 @@
 package notaria;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 
-
-public class InsertaDatosEnLaTabla { 
+public class InsertaDatosEnLaTabla {
     /**
      * Método para ingresar un nuevo cliente en la tabla clientes
      * Primero tiene que comprobar si ese cliente existe en la base de datos
@@ -23,25 +25,93 @@ public class InsertaDatosEnLaTabla {
      * 
      * @author Alba Parrilla
      */
-	public void ingresarDatos(){
+	public void ingresarDatos() {
         String sql;
         ConexionMysql mysql = new ConexionMysql();
-        Connection conn;
+        Connection conn = null;
         Statement s;
         ResultSet rs;
+        String codigoClienteRegistrado = "";
         try {
-            
         	//Se realiza la conexión y siguientes pasos
-        	
-        	//TODO se pide el codCli y se busca en la tabla de la BBDD
+            conn = mysql.Conectar();
+            s = conn.createStatement();
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Ingrese el codigo de cliente");
+            String codigoCliente = scan.nextLine();
+
+            SeleccionaDatosDeTabla seleccionaDatosDeTabla = new SeleccionaDatosDeTabla();
+            if (seleccionaDatosDeTabla.consultarCodCliente(codigoCliente)) {
+                System.out.println("--!!BIENVENIDO!!--");
+                rs = s.executeQuery("SELECT cod_Cliente FROM clientes WHERE cod_Cliente = " + codigoCliente);
+                rs.next();
+                codigoClienteRegistrado = rs.getString(0);
+            } else {
+                System.out.println(":( Parece que no estas en nuestra base de datos, vamos a registrarte!");
+                System.out.println(">Ingrese su codigo de cliente:");
+                String codigo = scan.nextLine();
+                System.out.println(">Ingrese su nombre:");
+                String nombre = scan.nextLine();
+                System.out.println(">Ingrese su telefono:");
+                String telefono = scan.nextLine();
+                s.executeUpdate(String.format(
+                        "INSERT INTO clientes VALUES('%s', '%s','%s')",
+                        codigo,
+                        nombre,
+                        telefono
+                ));
+                codigoClienteRegistrado = codigo;
+                System.out.println("--Enhorabuena! Has sido registrado--");
+            }
         	
         	//TODO se procede a pedir los datos para introducirlos en la BBDD
-        	
+            System.out.println("Vamos a ingresar datos en la tabla ESCRITURAS :)\n");
+            System.out.println(">Ingrese el codigo de escritura: ");
+            String codigoEscritura = scan.nextLine();
+            System.out.println(">Ingrese el tipo de escritura");
+            String tipoEscritura = scan.nextLine();
+            System.out.println(">Ingrese el nombre de la ficha");
+            String nombreFicha = scan.nextLine();
+            System.out.println(">Ingrese el numero de intervencion");
+            int numeroIntervencion = -1;
+
+            while (true) {
+                try {
+                    numeroIntervencion = scan.nextInt();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("!!!!!INTRODUCE BIEN LOS PARAMETROS >:(!!!!!");
+                }
+            }
+
+            s = conn.createStatement();
+            s.executeUpdate(
+                    String.format("INSERT INTO escrituras VALUES('%s','%s','%s','%s')",
+                            codigoEscritura,
+                            tipoEscritura,
+                            nombreFicha,
+                            numeroIntervencion
+                    ));
+            System.out.println("--DATOS INGRESADOS CORRECTAMENTE EN ESCRITURAS :)--");
+
+            System.out.println("Ahora es hora de ingresar datos en la tabla ESCCLI :D\n");
+            System.out.println(">Ingrese el codigo: ");
+            String codigoEsccli = scan.nextLine();
+            System.out.println(">Ingrese el codigo de escritura");
+            int codnnEscritura = -1;
+            s.executeUpdate(
+                    String.format("INSERT INTO esccli VALUES('%s','%s','%s')",
+                            codigoEsccli,
+                            codigoClienteRegistrado,
+                            codnnEscritura
+                    ));
+
         	
         } catch(Exception e) { 
             System.err.println ("Se han encontrado errores... "); 
         } finally {    
-            //TODO realizar desconexión BBDD
+            mysql.desconectar(conn);
         }
     }
 }  
